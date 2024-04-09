bf7279890d50b1d4d28b86d83237a66c9e738736
oscar <oscar@LAPTOP-G1N9MG0D>
2024-04-08 18:47:01 -0700

    added student model, controller, and index view

---
+++ COMP003B.Assignment3/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿// TODO: referrence Models namespace
+using COMP003B.Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private static List<Student> _students = new List<Student>();
+        // GET: Students
+        public IActionResult Index()
+        {
+            return View(_students);
+        }
+    }
+}
