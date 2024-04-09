0d0932c11be60d7e48ffe7c7819964db27f57df6
oscar <oscar@LAPTOP-G1N9MG0D>
2024-04-08 18:55:43 -0700

    modified default routing convention

--- COMP003B.Assignment3/Program.cs
+++ COMP003B.Assignment3/Program.cs
@@ -26,6 +26,7 @@ namespace COMP003B.Assignment3
 
             app.UseAuthorization();
 
+            // TODO: modify controller route to default to StudentsController
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Students}/{action=Index}/{id?}");
