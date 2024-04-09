da849c061e1c67c8d53a8dc79b483b48352eea9d
oscar <oscar@LAPTOP-G1N9MG0D>
2024-04-08 19:50:07 -0700

    updated

--- COMP003B.Assignment3/Models/Student.cs
+++ COMP003B.Assignment3/Models/Student.cs
@@ -11,10 +11,11 @@ namespace COMP003B.Assignment3.Models
         public int Id { get; set; }
         [Required]
         [StringLength(100)]
+        [Display(Name="Full Name")]
         public string Name { get; set; }
         [Required]
         [Range(0, 120)]
-        public int Age { get; set; } = 0;
+        public int Age { get; set; }
        
     }
 }
